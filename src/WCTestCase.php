37c7efef13595048d3de3453b77b52c512a88243
Niels de Blaauw <niels@level-level.com>
2024-03-15 17:02:37 +0100

    Restore instances after test runs

--- src/WCTestCase.php
+++ src/WCTestCase.php
@@ -5,6 +5,11 @@ namespace LevelLevel\WPBrowserWooCommerce;
 use Codeception\TestCase\WPTestCase;
 
 class WCTestCase extends WPTestCase {
+	private $original_cart;
+	private $original_session;
+	private $original_customer;
+	private $original_query;
+
 	/**
 	 * @return WC_UnitTest_Factory
 	 */
@@ -14,5 +19,23 @@ class WCTestCase extends WPTestCase {
 			$factory = new WC_UnitTest_Factory();
 		}
 		return $factory;
+	}
+
+	public function _setUp()
+	{
+		parent::_setUp();
+		$this->original_cart = WC()->cart;
+		$this->original_session = WC()->session;
+		$this->original_customer = WC()->customer;
+		$this->original_query = WC()->query;
+	}
+
+	public function _tearDown()
+	{
+		parent::_tearDown();
+		WC()->cart = $this->original_cart;
+		WC()->session = $this->original_session;
+		WC()->customer = $this->original_customer;
+		WC()->query = $this->original_query;
 	}
 }
